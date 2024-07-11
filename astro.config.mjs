03dac22c0a20511016679a47eb459e29a97995d7
o0v0 <3535516703@qq.com>
2024-07-11 12:33:19 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ function customRehypeLazyLoadImage() {
 }
 
 export default defineConfig({
-  site: 'https://astro-yi-nu.vercel.app',
+  site: 'https://astroyi.vercel.app',
   integrations: [sitemap(), tailwind(), solid(), expressiveCode({
     plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
     themes: ["github-dark", "github-light"],
