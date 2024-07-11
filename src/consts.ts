91ce7b26c15df75a792b415304ee4d2bdea6aadb
o0v0 <3535516703@qq.com>
2024-07-11 12:18:46 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -38,7 +38,7 @@ export const site = {
  */
 export const config = {
   busuanzi: false,
-  lang: 'en', // en | zh-cn
+  lang: 'zh-cn', // en | zh-cn
   codeFoldingStartLines: 16, // Need to re-run the project to take effect
   ga: false // If you want to integrate with Google Analytics, just enter your GA-ID here.
 }
